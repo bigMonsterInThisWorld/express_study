61861feba5fa1efc8d324ecd5fe3644d4dafdee1
15308077613 <15308077613@163.com>
2018-04-30 11:09:48 +0800

    项目部分修改

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+// router.get('/demo',(req,res,next)=>{
+//   res.send('demo');
+// });
 
 module.exports = router;
